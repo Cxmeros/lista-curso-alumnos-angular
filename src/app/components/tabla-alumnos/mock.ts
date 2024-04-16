cbc23c4549254b4984221c2dd4bb9d65fa6ced8b
Cxmeros <cxmeros118@gmail.com>
2024-04-15 21:43:31 -0600

    Tablas de cursos y alumnos con typescript

---
+++ src/app/components/tabla-alumnos/mock.ts
@@ -0,0 +1,19 @@
+import { Alumno } from "src/app/models/alumno-to";
+
+export const alumnos: Alumno[] = [
+  {
+    nombre: 'Juan',
+    apellido: 'Perez',
+    promedio: 8.5
+  },
+  {
+    nombre: 'Maria',
+    apellido: 'Gomez',
+    promedio: 9.0
+  },
+  {
+    nombre: 'Pedro',
+    apellido: 'Jimenez',
+    promedio: 7.0
+  }
+]
---
+++ src/app/components/tabla-cursos/mock.ts
@@ -0,0 +1,19 @@
+import { Curso } from "src/app/models/curso-to";
+
+export const cursos: Curso[] = [
+  {
+    materia: 'JavaScript',
+    dificultad: 6,
+    requiere: 'Html y Css'
+  },
+  {
+    materia: 'Angular',
+    dificultad: 9,
+    requiere: 'JavaScript y TypeScript'
+  },
+  {
+    materia: 'React',
+    dificultad: 7,
+    requiere: 'JavaScript'
+  }
+]
